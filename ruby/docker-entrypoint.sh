c363be1e5ac4bf922a7ce2279ae57cc8d89b669b
shaobo.zheng <shaobo.zheng@xuncetech.com>
2019-05-09 16:27:32 +0800

    del files

--- ruby/docker-entrypoint.sh
+++ ruby/docker-entrypoint.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-#echo "yes" | ruby /usr/local/bin/redis-trib.rb create --replicas 1 192.168.100.2:6380 192.168.100.3:6381 192.168.100.4:6382 192.168.100.5:6383 192.168.100.6:6384 192.168.100.7:6385
-#echo "yes" | redis-cli --cluster create 192.168.100.2:6380 192.168.100.3:6381 192.168.100.4:6382 192.168.100.5:6383 192.168.100.6:6384 192.168.100.7:6385 --cluster-replicas 1
 echo "yes" | redis-cli --cluster create ${ADDRD} --cluster-replicas 1
